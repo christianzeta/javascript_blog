0add1e5551686e32443df72f62d8b03c55229ea1
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-01-20 16:18:23 +0100

    initial layout

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+        FELSÖKNING
+        Det första jag gör är att implemenetera "use strict"; i början av filen så att det verkar globalt. Anledningen är för att inte tillåta 
+        dåligt syntax som t.ex. att skapa variabler utan att deklarera dem, samt för att generera errors vid misstag.
+
+*/ 
+
+"use strict";
